d39d4cb6a96a81e61a4b0b61871bc17df6d3f3bc
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-02-14 18:39:02 +0100

    add not working ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 16:13:13 by fgabler           #+#    #+#             */
+/*   Updated: 2024/02/14 18:24:55 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+static void	arg_check(int ac, int *error);
+static void	capitalice_string(char ***av, int error);
+static void	capitalice_line(char **line);
+static void	print_input(char **av, int error);
+
+int	main(int ac, char **av)
+{
+	int	error = false;
+
+	arg_check(ac, &error);
+	capitalice_string(&av, error);
+	print_input(av, error);
+	return (0);
+}
+
+static void	arg_check(int ac, int *error)
+{
+	std::string		no_argument = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+
+	if (ac > 1)
+		return ;
+	*error = true;
+	std::cout << no_argument << std::endl;
+}
+
+static void	capitalice_string(char ***av, int error)
+{
+	int	i;
+
+	if (error == true)
+		return ;
+	i = 1;
+	while ((*av)[i] != NULL)
+	{
+		capitalice_line(&(*av)[i]);
+		i++;
+	}
+}
+
+static void	capitalice_line(char **line)
+{
+	for(int i = 0; (*line)[i] != '\0'; i++)
+	{
+		*(line)[i] = std::toupper(*(line)[i]);
+		std::cout << "string " << (*line) << std::endl;
+	}
+}
+
+static void	print_input(char **av, int error)
+{
+	int	i;
+
+	i = 1;
+	if (error == true)
+		return ;
+
+	while (av[i] != NULL)
+	{
+		i++;
+	}
+}
