39e74e614b0c28817bdb1a21256832bc2988e9e7
fritz-gabler <fritzlmgaebler@gmail.com>
2024-04-05 13:01:43 +0200

    add amost fineshed cpp02 ex02

---
+++ CPP_02/ex02/include/Fixed.hpp
@@ -0,0 +1,53 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	public:
+		//Constructors
+		Fixed( void );
+		Fixed(	const Fixed &fixed );
+		~Fixed( void );
+		Fixed( const int intNumber );
+		Fixed( const float floatNumber );
+
+		//Functions
+		int			getRawBits( void );
+		void		setRawBits( int const raw );
+		int			toInt( void ) const;
+		float		toFloat( void ) const;
+		static Fixed		&min( Fixed &first, Fixed &second);
+		static const Fixed	&min( const Fixed &first, const Fixed &second);
+		static Fixed		&max( Fixed &first, Fixed &second);
+		static const Fixed	&max( const Fixed &first, const Fixed &second);
+
+		//Overloads
+		Fixed &operator = ( const Fixed &other );
+
+		bool operator > ( const Fixed &other );
+		bool operator < ( const Fixed &other );
+		bool operator >= ( const Fixed &other );
+		bool operator <= ( const Fixed &other );
+		bool operator == ( const Fixed &other );
+		bool operator != ( const Fixed &other );
+
+		Fixed operator + ( const Fixed &other );
+		Fixed operator - ( const Fixed &other );
+		Fixed operator * ( const Fixed &other );
+		Fixed operator / ( const Fixed &other );
+
+		Fixed &operator ++ ( void );
+		Fixed operator ++ ( int );
+		Fixed &operator -- ( void );
+		Fixed operator -- ( int );
+
+	private:
+		int					_fixedPointNumber;
+		static const int	_fractionalValue = 8;
+};
+
+std::ostream &operator<<(std::ostream &outStream, const Fixed &fixed);
+
+#endif
