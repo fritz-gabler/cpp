6cd9c31a8410852d0f1d6da94c5b3c7fa6110051
fritz-gabler <fritzlmgaebler@gmail.com>
2024-04-09 10:56:31 +0200

    add working cpp02 ex03

--- CPP_02/ex03/src/binarySpacePartitioning/bsp.cpp
+++ CPP_02/ex03/src/binarySpacePartitioning/bsp.cpp
@@ -6,33 +6,70 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 10:32:56 by fgabler           #+#    #+#             */
-/*   Updated: 2024/04/08 18:51:38 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/09 10:54:57 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
+static void TriangleCalc(Point const a, Point const b, Point const c, float &area);
 static void	calcLineBetweenTwoPoints(Point const one, Point const two, float &line);
+static void calcAreaOfTriangle(float line[3], float &area);
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	float	aLine;
-	float	bLine;
-	float	cLine;
+	float	areaOrginal;
+	float	partOfTriangle[3];
+	float	areaToCheck;
+	float	epsilon;
 
-	calcLineBetweenTwoPoints(a, b, aLine);
-	calcLineBetweenTwoPoints(c, b, bLine);
-	calcLineBetweenTwoPoints(a, c, cLine);
-	std::cout << "Line a: " << aLine << std::endl;
-	std::cout << "Line b: " << bLine << std::endl;
-	std::cout << "Line c: " << cLine << std::endl;
-	(void) point;
-	(void) c;
-	return (true);
+	TriangleCalc(a, b, c, areaOrginal);
+	TriangleCalc(a, b, point, partOfTriangle[A]);
+	TriangleCalc(a, c, point, partOfTriangle[B]);
+	TriangleCalc(b, c, point, partOfTriangle[C]);
+
+	epsilon = std::numeric_limits<float>::epsilon();
+	areaToCheck = partOfTriangle[A] + partOfTriangle[B] + partOfTriangle[C];
+	if (areaOrginal - areaToCheck < epsilon)
+		return (true);
+	return (false);
 }
 
+static void TriangleCalc(Point const a, Point const b, Point const c, float &area)
+{ float	line[3];
+	calcLineBetweenTwoPoints(a, b, line[A]);
+	calcLineBetweenTwoPoints(c, b, line[B]);
+	calcLineBetweenTwoPoints(a, c, line[C]);
+	calcAreaOfTriangle(line, area);
+	/*
+	std::cout << "line a: "
+		<< line[A]
+		<< std::endl
+		<< "line b: "
+		<< line[B]
+		<< std::endl
+		<< "line c: "
+		<< line[C]
+		<< std::endl;
+	*/
+}
 static void	calcLineBetweenTwoPoints(Point const one, Point const two, float &line)
 {
-	line = std::pow(2, one.getX().toFloat() - two.getX().toFloat()) + std::pow(2, one.getY().toFloat() - two.getY().toFloat());
+	float	calculateX;
+	float	calculateY;
+
+
+	calculateX = std::pow(one.getX().toFloat() - two.getX().toFloat(), 2);
+	calculateY = std::pow(one.getY().toFloat() - two.getY().toFloat(), 2);
+	line = calculateX + calculateY;
 	line = std::sqrt(line);
+}
+
+static void calcAreaOfTriangle(float line[3], float &area)
+{
+	float	s; //semiperimeter
+
+	s = (line[A] + line[B] + line[C]) / 2;
+	area = s * (s - line[A]) * (s - line[B]) * (s - line[C]);
+	area = std::sqrt(area);
 }
--- CPP_02/ex03/src/main.cpp
+++ CPP_02/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:55:34 by fgabler           #+#    #+#             */
-/*   Updated: 2024/04/08 18:46:59 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/09 10:37:33 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int main( void )
 {
-	Point	Apoint(4.5f, 2.5f);
-	Point	Bpoint(7.5f, 8.5f);
-	Point	Cpoint(1.5f, 0.5f);
+	Point	Apoint(0, 2);
+	Point	Bpoint(8, 2);
+	Point	Cpoint(6, 8);
 
-	bsp(Apoint, Bpoint, Cpoint, Cpoint);
+	Point	checkPointOne(2.5f, 3.0);
+	if (bsp(Apoint, Bpoint, Cpoint, checkPointOne) == true)
+		std::cout << "point is in range of triangle" << std::endl;
+	else
+		std::cout << "point is NOT in range of triangle" << std::endl;
 	return 0;
 }
 
