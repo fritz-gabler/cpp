41bf604ddf9467058a2e86cc21ef22d9f0f84a41
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-16 15:35:04 +0200

    add dog class and cpp file

--- CPP_04/ex00/include/objects/dog.hpp
+++ CPP_04/ex00/include/objects/dog.hpp
@@ -1,6 +1,15 @@
 #ifndef DOG_HPP
 #define DOG_HPP
 
+#include "header.hpp"
 
+class	Dog : public Animal
+{
+	public:
+		Dog( void );
+		Dog( const Dog &other );
+		Dog &operator = ( const Dog &other );
+		~Dog( void );
+};
 
 #endif
--- CPP_04/ex00/src/objectFunctions/dog.cpp
+++ CPP_04/ex00/src/objectFunctions/dog.cpp
@@ -6,9 +6,33 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 14:57:26 by fgabler           #+#    #+#             */
-/*   Updated: 2024/04/16 14:57:48 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/16 15:34:22 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
+Dog::Dog( void )
+{
+	this->_type = "Dog";
+	std::cout << "[ DOG ] Default constructor called" << std::endl;
+}
+
+Dog::Dog( const Dog &other )
+{
+	this->_type = other._type;
+	std::cout << "[ DOG ] Copy constructor called" << std::endl;
+}
+
+Dog &Dog::operator = (const Dog &other)
+{
+	if (this != &other)
+		this->_type = other._type;
+	std::cout << "[ DOG ] Copy assignment constructor called" << std::endl;
+	return (*this);
+}
+
+Dog::~Dog( void )
+{
+	std::cout << "[ DOG ] Destructor called" << std::endl;
+}
