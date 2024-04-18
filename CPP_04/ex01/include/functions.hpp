09bc0d9669fb44a03100be70f45e331181e26ced
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-18 19:54:51 +0200

    add functions header for new functions to put in

---
+++ CPP_04/ex01/include/functions.hpp
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_HPP
+#define  FUNCTIONS_HPP
+
+//CHECKS
+bool	inBrainCapacityRange(int number);
+
+//TESTS
+void	pdfTest( void );
+void	copyTest( void );
+void	boundaryTests( void );
+
+#endif
