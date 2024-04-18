28b629ee9dc25df9ae7ef9513677c4cd6a82ce05
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-18 19:50:08 +0200

    add brain functions to handle new ideas

--- CPP_04/ex01/include/objects/brain.hpp
+++ CPP_04/ex01/include/objects/brain.hpp
@@ -3,6 +3,8 @@
 
 #include "header.hpp"
 
+#define BRAIN_CAPACITY 100
+
 class	Brain
 {
 	public:
@@ -10,8 +12,14 @@ class	Brain
 		Brain( const Brain &other );
 		Brain &operator = ( const Brain &other );
 		~Brain( void );
+
+		void	saveNewIdea( const std::string &idea );
+		void	changeAnIdea( const std::string &idea, int ideaNumber );
+		void	printIdeas( void );
+		void	printAnSpecificIdea( int ideaNumber );
 	private:
-		std::string	_ideas[100];
+		int			_currentIdeaNumber;
+		std::string	_ideas[BRAIN_CAPACITY];
 };
 
 #endif
