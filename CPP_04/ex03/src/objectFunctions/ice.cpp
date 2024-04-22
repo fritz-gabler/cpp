c9ed2395ccdcbfacb942f70fa759ba2de425afbf
fritz-gabler <fritzlmgaebler@gmail.com>
2024-04-22 18:40:27 +0200

    add ice mamber functions

--- CPP_04/ex03/src/objectFunctions/ice.cpp
+++ CPP_04/ex03/src/objectFunctions/ice.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:35:58 by fgabler           #+#    #+#             */
-/*   Updated: 2024/04/22 14:31:23 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/22 17:54:37 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ Ice::Ice( const Ice &other )
 	//std::cout << "[ ICE ] Constructor with arguments called" << std::endl;
 }
 
-Ice Ice::operator = ( const Ice &other )
+Ice &Ice::operator = ( const Ice &other )
 {
 	void (other);
 	return (*this);
@@ -37,4 +37,18 @@ Ice Ice::operator = ( const Ice &other )
 Ice::~Ice( void )
 {
 	//std::cout << "[ ICE ] Destructor called" << std::endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////MEMBER FUNCTIONS///////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+Ice		*Ice::clone( void ) const
+{
+	return (new Ice(*this));
+}
+
+void	Ice::use( ICharacter &target )
+{
+	std::cout << "* shoots an ice bolt at " << target.getName() << "*\n";
 }
