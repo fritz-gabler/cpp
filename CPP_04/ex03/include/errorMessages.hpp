d95d5244c0380e34c65aeae53148b1d971aaf516
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-23 11:30:19 +0200

    add ifndef instad of ifdef to error message file in cp04 ex03

--- CPP_04/ex03/include/errorMessages.hpp
+++ CPP_04/ex03/include/errorMessages.hpp
@@ -1,12 +1,12 @@
-#ifdef ERROR_MESSAGE
+#ifndef ERROR_MESSAGE
 #define ERROR_MESSAGE
 
-# define NOT_IN_INDEX_RANGE			"ERROR: Index not in Inventory range\n"
-# define NO_SPACE_IN_INVENTORY		"WARNING: No space left in inventory\n";
-# define INPUT_MATERIA_IS_NULL		"ERROR: Inputted materia is NULL\n";
-# define NO_MATERIA_AT_THAT_INDEX	"WARNING: No materia at that index\n"
-# define CANT_USE_USE_IN_AMATERIA	"ERROR: Abstract base Member function use can't be use\n"
-# define MATERIA_IS_NULL			"WARNING: Inputted materia is NULL\n";
-# define MATERIA_CANDT_BE_ADDED		"WARINGI: Inputted materia couldn't be added\n"
+# define UNABLE_TO_USE_USE_FUNC			"ERROR: Abstract base Member function use can't be use\n"
+# define NOT_IN_INDEX_RANGE				"ERROR: Index not in Inventory range\n"
+# define NO_SPACE_IN_INVENTORY			"WARNING: No space left in inventory\n";
+# define INPUT_MATERIA_IS_NULL			"ERROR: Inputted materia is NULL\n";
+# define NO_MATERIA_AT_THAT_INDEX		"WARNING: No materia at that index\n"
+# define MATERIA_IS_NULL				"WARNING: Inputted materia is NULL\n";
+# define MATERIA_CANDT_BE_ADDED			"WARINGI: Inputted materia couldn't be added\n"
 
 #endif
