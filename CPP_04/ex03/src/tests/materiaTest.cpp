44db7d3ed7f1be6d2e105ae2fd7c06ff1ac50ad9
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-23 15:04:20 +0200

    Add delete in test so no memory leaks accour

--- CPP_04/ex03/src/tests/materiaTest.cpp
+++ CPP_04/ex03/src/tests/materiaTest.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:42:46 by fgabler           #+#    #+#             */
-/*   Updated: 2024/04/23 14:12:19 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/23 15:02:42 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ void	materiaTest( void )
 {
 	std::cout << "\n------------------MATERIA TEST------------------\n";
 	MateriaSource materia;
+	Cure	*cureToLearn[INVENTORY_VOLUME + 1];
 
+
+	for (int i = 0 ; i <= INVENTORY_VOLUME ; i++ )
+		cureToLearn[i] = new Cure;
 
 	std::cout << "\nTry to learn more than four materials:\n";
 	for (int i = 0 ; i <= INVENTORY_VOLUME ; i++ )
@@ -25,11 +29,12 @@ void	materiaTest( void )
 		std::cout << "i: " << i << " | ";
 		if (i == 4)
 			std::cout << std::endl;
-		materia.learnMateria(new Cure());
+		materia.learnMateria(cureToLearn[i]);
 	}
 	std::cout << std::endl;
 
 	std::cout << "\nTry to create materia ice:\n";
 	materia.createMateria("ice");
 	std::cout << std::endl;
+	delete cureToLearn[4];
 }
