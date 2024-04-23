c85cf5c51dc006b30666c47e4e058f7460a4f92d
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-23 16:56:58 +0200

    use c style strncmp not some made up function std::strncpm that does not exixt

--- CPP_01/ex04/src/fileManipulation/isInputString.cpp
+++ CPP_01/ex04/src/fileManipulation/isInputString.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 08:27:06 by fgabler           #+#    #+#             */
-/*   Updated: 2024/03/30 13:21:18 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/23 15:42:09 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool	isInputString(std::string fileLine, Input &input)
 	int	strLeng;
 
 	strLeng = input.stringOne.length();
-	if (std::strncmp((fileLine.c_str()), input.stringOne.c_str(), strLeng) == FoundStr)
+	if (strncmp((fileLine.c_str()), input.stringOne.c_str(), strLeng) == FoundStr)
 		return (true);
 	return (false);
 }
