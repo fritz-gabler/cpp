c7a15b67f89cdfbbd8e8b5c37fc07c52243791de
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-23 10:59:57 +0200

    add main content

---
+++ CPP_04/ex03/src/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 10:46:56 by fgabler           #+#    #+#             */
+/*   Updated: 2024/04/23 10:48:13 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.hpp"
+
+int main( void )
+{
+	IMateriaSource* src = new MateriaSource();
+
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+	return 0;
+}
+
