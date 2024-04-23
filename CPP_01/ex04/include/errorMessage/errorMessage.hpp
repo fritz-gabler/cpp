2d4b1381a4029ebe1f2a32c5ef11cece42256fb6
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-23 16:56:09 +0200

    change error message

--- CPP_01/ex04/include/errorMessage/errorMessage.hpp
+++ CPP_01/ex04/include/errorMessage/errorMessage.hpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 12:01:56 by fgabler           #+#    #+#             */
-/*   Updated: 2024/03/18 10:52:38 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/04/23 15:51:36 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include "header.hpp"
 
-# define WrongNumberOfArgs				"Wrong number of input"
+# define WrongNumberOfArgs				"Wrong number of input. Please input a filename and two strings, s1 and s2"
 # define FileCantBeOpen					"Inputed file cant't be opend"
 
 #endif
