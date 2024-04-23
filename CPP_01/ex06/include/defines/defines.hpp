93b14a3ede4eefeeafceefcf8067811b95acf476
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-23 16:59:44 +0200

    change little not mentionable stuff

--- CPP_01/ex06/include/defines/defines.hpp
+++ CPP_01/ex06/include/defines/defines.hpp
@@ -24,9 +24,9 @@ enum	EcaseLevel
 # define	INFO_MESSAGE		"cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
 # define	WARNING_MESSAGE		"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month"
 # define	ERROR_MESSAGE		"This is unacceptable! I want to speak to the manager now."
-# define		DEFAULT_MESSAGE		"[ Probably complaining about insignificant problems ]"
+# define	DEFAULT_MESSAGE		"[ Probably complaining about insignificant problems ]"
 
-#define		WRONG_NB_OF_ARGS	"Wrong number of arguments"
+#define		WRONG_NB_OF_ARGS	"Wrong number of arguments. Please input DEBUG, INFO, WARNING or ERROR"
 #define		WRONG_LEVEL_NAME	"Wrong Level Name, input DEBUG, INFO, WARNING or ERROR"
 
 #endif
