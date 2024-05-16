686e20298cf093884b267090a13bd9c97b19b574
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-05-16 17:13:30 +0200

    add a little bit of cpp06 ex00

--- CPP_06/ex00/include/objects/ScalarConverter.hpp
+++ CPP_06/ex00/include/objects/ScalarConverter.hpp
@@ -1,9 +1,23 @@
 #ifndef  SCALAR_CONVERTER
 #define  SCALAR_CONVERTER
 
+#include <iostream>
+
 class ScalarConverter
 {
+  public:
+    //Constructor and Destructors
+    ScalarConverter(void);
+    ScalarConverter(const ScalarConverter &other);
+    ScalarConverter operator = (const ScalarConverter &other);
+    ~ScalarConverter(void);
 
+    //Methods
+    static void convert(const std::string &);
+    static void char_print_if_possible(const std::string &);
+    static void int_print_if_possible(const std::string &);
+    static void float_print_if_possible(const std::string &);
+    static void double_print_if_possible(const std::string &);
 };
 
 #endif
--- CPP_06/ex00/src/objectFunctions/ScalarConverter.cpp
+++ CPP_06/ex00/src/objectFunctions/ScalarConverter.cpp
@@ -6,8 +6,31 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:01:19 by fgabler           #+#    #+#             */
-/*   Updated: 2024/05/16 12:01:21 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/05/16 16:39:32 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
 
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////CONSTRUCTOR AND DESTRUCTOR/////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &) {}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &) {
+  return (*this);
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////MEMBER FUNCTIONS///////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+static void ScalarConverter::convert(const std::string &input)
+{
+  
+}
