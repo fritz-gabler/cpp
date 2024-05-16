13282674591bcdb41c5043bccf6a132f0871fe21
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-05-16 17:20:53 +0200

    add a little bit of input parsing

--- CPP_06/ex00/src/input_parsing/input_parsing.cpp
+++ CPP_06/ex00/src/input_parsing/input_parsing.cpp
@@ -6,10 +6,17 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:50:52 by fgabler           #+#    #+#             */
-/*   Updated: 2024/05/16 17:11:54 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/05/16 17:19:48 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-void input_parsing()
+bool input_parsing(int ac, char **av)
+{
+  if(ac != 2)
+  {
+
+  }
+    
+}
