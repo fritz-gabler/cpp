8dcbddf1335981e10860d2410f12c151408fd58f
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-05-19 10:43:08 +0200

    fix input parsing

--- CPP_06/ex00/src/input_parsing/input_parsing.cpp
+++ CPP_06/ex00/src/input_parsing/input_parsing.cpp
@@ -6,22 +6,22 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:50:52 by fgabler           #+#    #+#             */
-/*   Updated: 2024/05/17 17:46:55 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/05/19 10:41:16 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 #include <cctype>
 
-static bool is_str_number(char *str);
-static bool is_str_alpha(char *str);
+static bool is_number_in_str(char *str);
+static bool is_alpha_in_str(char *str);
 
 bool input_parsing(int ac, char **av) {
   if (ac != 2) {
     log("Wrong number of parameter: ./scalar_converter oneArgument", ERROR);
     return (false);
   }
-  else if (is_str_number(av[1]) == true && is_str_alpha(av[1]) == true)
+  if (is_number_in_str(av[1]) == true && is_alpha_in_str(av[1]) == true)
   {
     log("Mixed input: mixing numbers and alphabeticals is invalide", ERROR);
     return (false);
@@ -29,22 +29,23 @@ bool input_parsing(int ac, char **av) {
   return (true);
 }
 
-static bool is_str_number(char *str)
+static bool is_number_in_str(char *str)
 {
   for (int i = 0; str[i] != '\0'; i++)
   {
-    if (std::isdigit(str[i]) == false)
-      return (false);
+    if (std::isdigit(str[i]) != false)
+      return (true);
   }
-  return (true);
+  return (false);
 }
 
-static bool is_str_alpha(char *str)
+static bool is_alpha_in_str(char *str)
 {
   for (int i = 0; str[i] != '\0'; i++)
   {
-    if (std::isalpha(str[i]) == false)
-      return (false);
+    if (std::isalpha(str[i]) != false)
+      return (true);
   }
-  return (true);
+  std::cout << "str numb: true\n";
+  return (false);
 }
