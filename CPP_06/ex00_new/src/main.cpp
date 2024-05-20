feb43d75307e2c1c562d86e55aa1161db2413b3e
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-05-20 12:06:51 +0200

    add new main strucute

--- CPP_06/ex00_new/src/main.cpp
+++ CPP_06/ex00_new/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:51:11 by fgabler           #+#    #+#             */
-/*   Updated: 2024/05/17 11:21:13 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/05/20 10:18:16 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 
 int main(int ac, char **av)
 {
-  if (input_parsing(ac, av) == false)
+  if (ac != 2)
     return (0);
   ScalarConverter::convert(av[1]);
-  static_cast<void>(av);
   return (0);
 }
