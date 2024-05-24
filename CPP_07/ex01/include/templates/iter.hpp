f120a1bb72f0a99bc8d54080c35e0f75e4fdc31a
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-05-24 13:06:04 +0200

    add new cpp07 ex01

---
+++ CPP_07/ex01/include/templates/iter.hpp
@@ -0,0 +1,15 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template<typename Ptr_to_array, typename Array_size, typename Function>
+void iter(Ptr_to_array *ptr, Array_size size, Function(*function)(Ptr_to_array))
+{
+  if (ptr == NULL || size < 1 || function == NULL)
+    return ;
+  for(int i = 0; i < size; i++)
+  {
+    function(ptr[i]);
+  }
+}
+
+#endif
