a90ef74a8bb938e0e7bbc491f2a5c10d118fdd30
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-05-25 16:29:55 +0200

    add main test from subject

--- CPP_07/ex02/include/functions.hpp
+++ CPP_07/ex02/include/functions.hpp
@@ -1,9 +1,14 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 
+#include "define.hpp"
+
 //LOG
 void log(std::string message, t_log_level level);
 
+//TESTS
+void selfe_created_test();
+int subject_test();
 
 #endif
 
---
+++ CPP_07/ex02/src/tests/subject_test.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   subject_test.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 15:30:00 by fgabler           #+#    #+#             */
+/*   Updated: 2024/05/25 15:38:41 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+#include "header.hpp"
+
+#define MAX_VAL 750
+
+int subject_test()
+{
+  Array<int> numbers(MAX_VAL);
+  int* mirror = new int[MAX_VAL];
+  srand(time(NULL));
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    const int value = rand();
+    numbers[i] = value;
+    mirror[i] = value;
+  }
+  //SCOPE
+  {
+    Array<int> tmp = numbers;
+    Array<int> test(tmp);
+  }
+
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    if (mirror[i] != numbers[i])
+    {
+      std::cerr << "didn't save the same value!!" << std::endl;
+      return 1;
+    }
+  }
+  try
+  {
+    numbers[-2] = 0;
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  try
+  {
+    numbers[MAX_VAL] = 0;
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    numbers[i] = rand();
+  }
+  delete [] mirror;//
+  return 0;
+}
