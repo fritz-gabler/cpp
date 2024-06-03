9cf891b0241082536e17dfe981ce08116e6c6cb3
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-06-03 17:57:22 +0200

    add correct pointer usage

--- CPP_08/ex01/src/object_functions/Span.cpp
+++ CPP_08/ex01/src/object_functions/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 10:19:38 by fgabler           #+#    #+#             */
-/*   Updated: 2024/06/03 14:58:44 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/06/03 17:32:46 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 Span::Span(const Span &other)
 {
-  &this = other;
+  *this = other;
 }
 
-Span Span::operator = (const Span &other)
+Span &Span::operator = (const Span &other)
 {
   if (this != &other)
   {
@@ -31,3 +31,5 @@ Span::Span(unsigned int N) : size_(N)
 {
   numbers_.reverse(N);
 }
+
+
