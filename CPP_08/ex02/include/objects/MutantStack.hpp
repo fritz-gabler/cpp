e86fb95e3c40c84d11f67b437b66adc0f2dbdc00
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-06-20 15:58:13 +0200

    add new ex 02 in cpp08

---
+++ CPP_08/ex02/include/objects/MutantStack.hpp
@@ -0,0 +1,75 @@
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+
+#include <stack>
+#include <iterator>
+#include <deque>
+#include <iostream>
+
+template<typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+  public:
+    MutantStack() : std::stack<T, Container>() {};
+    MutantStack(const MutantStack &other) : std::stack<T, Container>(other){};
+    MutantStack &operator = (const MutantStack &other)
+    {
+      std::stack<T, Container>::operator=(other);
+      return (*this);
+    }
+
+    typedef typename Container::iterator iterator;
+    typedef typename  Container::const_iterator const_iterator;
+    typedef typename  Container::reverse_iterator reverse_iterator;
+    typedef typename  Container::const_reverse_iterator const_reverse_iterator;
+
+    iterator begin()
+    {
+      return (this->c.begin());
+    }
+    
+    iterator end()
+    {
+      return (this->c.end());
+    }
+
+    const_iterator begin() const
+    {
+      return (this->c.begin());
+    }
+
+    const_iterator end() const
+    {
+      return (this->c.end());
+    }
+
+    reverse_iterator rbegin()
+    {
+      return (this->c.rbegin());
+    }
+
+    reverse_iterator rend()
+    {
+      return (this->rend());
+    }
+
+    const_reverse_iterator rbegin() const
+    {
+      return (this->c.rbegin());
+    }
+
+    const_reverse_iterator rend() const
+    {
+      return (this->rend());
+    }
+};
+
+template <typename T, typename Container>
+std::ostream &operator << (std::ostream &ostream, const MutantStack<T, Container>& mutant_stack)
+{
+  typename MutantStack<T, Container>::const_reverse_iterator rit;
+
+  for (rit = mutant_stack.rbegin(); rit != mutant_stack.rend(); rit++)
+    ostream << *rit << std::endl;
+}
+#endif
---
+++ CPP_08/ex02/src/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 14:28:20 by fgabler           #+#    #+#             */
+/*   Updated: 2024/06/20 15:57:11 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+
+
+int main()
+{
+  MutantStack<int> mstack;
+
+  mstack.push(5);
+  mstack.push(17);
+
+  std::cout << mstack.top() << std::endl;
+
+  mstack.pop();
+
+  std::cout << mstack.size() << std::endl;
+
+  mstack.push(3);
+  mstack.push(5);
+  mstack.push(737);
+  mstack.push(0);
+
+  MutantStack<int>::iterator it = mstack.begin();
+  MutantStack<int>::iterator ite = mstack.end();
+
+  ++it;
+  --it;
+
+  while (it != ite)
+  {
+    std::cout << *it << std::endl;
+    ++it;
+  }
+  std::stack<int> s(mstack);
+  return 0;
+}
