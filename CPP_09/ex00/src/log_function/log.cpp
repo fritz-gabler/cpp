a8479970c7bfc3861be94f0870559727f483ad5f
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-06-27 11:00:25 +0200

    add new cpp09 structure no real code

---
+++ CPP_09/ex00/src/log_function/log.cpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   log.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 21:25:12 by fgabler           #+#    #+#             */
+/*   Updated: 2024/05/16 22:04:48 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "colors.hpp"
+#include <iostream>
+#include "define.hpp"
+
+static void set_color(t_colors color);
+static void output_log_level(t_log_level level);
+
+void log(std::string message, t_log_level level)
+{
+  output_log_level(level);
+  std::cout << message << std::endl;
+}
+
+static void output_log_level(t_log_level level)
+{
+  switch(level)
+  {
+    case TEST:
+      set_color(blue);
+      std::cout << "[TEST] ";
+      break;
+    case WARNING:
+      set_color(yellow);
+      std::cout << "[WARNING] ";
+      break;
+    case ERROR:
+      set_color(red);
+      std::cout << "[ERROR] ";
+      break;
+    case NOTE:
+      set_color(dark_green);
+      std::cout << "[NOTE] ";
+      break;
+    case EXCEPTION:
+      set_color(magenta);
+      std::cout << "[EXCEPTION] ";
+      break;
+  }
+  set_color(reset);
+}
+
+static void set_color(t_colors color)
+{
+  switch(color)
+  {
+    case red:
+      std::cout << RED;
+      break;
+    case dark_green:
+      std::cout << DARK_GREEN;
+      break;
+    case gress_green:
+      std::cout << GRASS_GREEN;
+      break;
+    case yellow:
+      std::cout << YELLOW;
+      break;
+    case magenta:
+      std::cout << MAGENTA;
+      break;
+    case blue:
+      std::cout << BLUE;
+      break;
+    case white:
+      std::cout << WHITE;
+      break;
+    case orange:
+      std::cout << ORANGE;
+      break;
+    case reset:
+      std::cout << RESET;
+      break;
+  }
+}
+
