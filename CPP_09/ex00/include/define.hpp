9b5c6a38154e83b27f7353e56b487a82fe505e90
fritz-gabler <fritzlmgaebler@gmail.com>
2024-07-01 08:52:17 +0200

    add new enum to define to check date better in cpp09 ex00

--- CPP_09/ex00/include/define.hpp
+++ CPP_09/ex00/include/define.hpp
@@ -1,9 +1,22 @@
 #ifndef DEFINE_HPP
 #define DEFINE_HPP
 
+////////////////////////////////////INCLUDES///////////////////////////////////
+
 #include "colors.hpp"
+
+////////////////////////////////////DEFINES////////////////////////////////////
 
 #define YEAR_ERROR "Invalid Date "
 #define RED_ERROR RED << "[ERROR] " << RESET
+
+
+enum e_date
+{
+  YEAR = 0,
+  MONTH = 1,
+  DAY = 2
+};
+
 
 #endif
