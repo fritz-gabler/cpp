0be4555632808ba8bc8b563f487f5eb5f1cb2a81
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-07-06 19:30:28 +0200

    add basic test in main in cpp09 ex02

---
+++ CPP_09/ex02/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 16:38:59 by fgabler           #+#    #+#             */
+/*   Updated: 2024/07/06 17:00:57 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.hpp"
+
+int main(int ac, char **av)
+{
+  if (ac < 2)
+  {
+    log(WRONG_ARGS, ERROR);
+    return (false);
+  }
+  else if (input_validation(av[1]) == false)
+    return (false);
+
+  std::vector<int> test;
+
+  test.push_back(3);
+  test.push_back(4);
+  test.push_back(4);
+  test.push_back(4);
+  test.push_back(4);
+  test.push_back(9);
+  test.push_back(2);
+  PmergeMe test_class(test);
+  return (true);
+}
