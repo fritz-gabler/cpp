77b6b70b09635d7a41f464fb2f94378609957abb
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-07-08 12:58:57 +0200

    add new function and tests to two public functions in cpp09 ex02

--- CPP_09/ex02/src/object_functions/PmergeMe.cpp
+++ CPP_09/ex02/src/object_functions/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 10:43:28 by fgabler           #+#    #+#             */
-/*   Updated: 2024/07/07 15:00:52 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/07/07 19:53:20 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,12 +71,22 @@ std::vector<unsigned int> &PmergeMe::deque_merge_insertion_sort()
 std::vector<unsigned int> &PmergeMe::vector_merge_insertion_sort()
 {
   create_internally_sorted_pairs(vector_to_sort_);
-  std::vector<std::vector<unsigned int> >::iterator it;
+  sort_larger_and_first_smmaler_nums_in_sequence(vector_to_sort_);
 
+  std::vector<std::vector<unsigned int> >::iterator it;
   for (it = vector_to_sort_.begin(); it != vector_to_sort_.end(); it++)
   {
     std::cout << "pair: " << (*it).front() << " " << (*it).back() << std::endl;
   }
+  std::cout << std::endl << std::endl;
+
+  std::vector<unsigned int>::iterator it_s;
+
+  for (it_s = sequence_.begin(); it_s != sequence_.end(); it_s++)
+  {
+    std::cout << *it_s << " ";
+  }
+  std::cout << std::endl;
   return (sequence_);
 }
 
@@ -87,5 +97,4 @@ void PmergeMe::save_possible_orphan_number()
   orphan_ = input_.back();
   input_.pop_back();
   return ;
-
 }
