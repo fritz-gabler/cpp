3dfb079c50b6f90233879ccd71200e2bcad9dcd7
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-07-08 12:59:18 +0200

    add tests to main in cpp09 ex02

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 16:38:59 by fgabler           #+#    #+#             */
-/*   Updated: 2024/07/06 21:42:15 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/07/07 18:36:30 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ int main(int ac, char **av)
 
   std::vector<unsigned int> test;
 
+  test.push_back(2);
+  test.push_back(1);
+  test.push_back(7);
   test.push_back(3);
   test.push_back(4);
-  test.push_back(4);
-  test.push_back(4);
-  test.push_back(4);
   test.push_back(9);
-  test.push_back(2);
   PmergeMe test_class(test);
+  test_class.vector_merge_insertion_sort();
   return (true);
 }
