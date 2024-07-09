b8c96f4d2ee8bd9de26f6a7777e3596bf7345a55
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-07-09 13:24:58 +0200

    add a test check in main cpp09 ex02

--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 16:38:59 by fgabler           #+#    #+#             */
-/*   Updated: 2024/07/07 18:36:30 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/07/09 11:20:54 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,11 @@ int main(int ac, char **av)
 
   std::vector<unsigned int> test;
 
-  test.push_back(2);
-  test.push_back(1);
-  test.push_back(7);
-  test.push_back(3);
-  test.push_back(4);
-  test.push_back(9);
+  for (int i = 0; i < 10; i++)
+  {
+    test.push_back(i + 1);
+  }
+  std::cout << std::endl;
   PmergeMe test_class(test);
   test_class.vector_merge_insertion_sort();
   return (true);
