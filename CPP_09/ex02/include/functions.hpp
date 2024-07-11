1db826df34df3f3222415aa5fd93276b0654ec12
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-07-11 09:57:57 +0200

    add function to stop time in cpp09 ex02

--- CPP_09/ex02/include/functions.hpp
+++ CPP_09/ex02/include/functions.hpp
@@ -12,12 +12,11 @@ void log(std::string message, t_log_level level);
 bool input_validation(char **av, int ac);
 
 //CONVERT
-void convert_input(int ac, char **av,
-                              std::vector<unsigned int> &converted_input);
+void convert_input(int ac, char **av, std::vector<unsigned int> &converted_input);
 
 //TIME
 void start_time(clock_t &time);
-void get_stopped_time(clock_t &time);
+void get_stopped_time(clock_t &time, double &duration);
 
 
 
--- CPP_09/ex02/src/time_mesurement/get_stopped_time.cpp
+++ CPP_09/ex02/src/time_mesurement/get_stopped_time.cpp
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_stoped_time.cpp                                :+:      :+:    :+:   */
+/*   get_stopped_time.cpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgabler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 21:47:27 by fgabler           #+#    #+#             */
-/*   Updated: 2024/07/10 21:48:39 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/07/11 09:53:04 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <time.h>
 
-void get_stopped_time(clock_t &time)
+void get_stopped_time(clock_t &start_time, double &duration)
 {
-  time = clock() - time;
+  clock_t end_time = clock();
+  duration = static_cast<double>(end_time - start_time)
+                                                / CLOCKS_PER_SEC * 1000000;
 }
